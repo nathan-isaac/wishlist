f906f5e01cf2920a571da0f4eb67870381b769bd
Nathan Isaac <jarvis.git@pm.me>
2024-02-20 21:38:44 -0800

    add some styles to share page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./internal/views/**/*.{html,js,templ}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
