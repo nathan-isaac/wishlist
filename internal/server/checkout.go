a6724c1b8fc03fd8460d5b68ff00b49b46a8606a
Nathan Isaac <jarvis.account@pm.me>
2024-03-27 08:05:19 -0700

    create checkout creation queries

--- internal/server/checkout.go
+++ internal/server/checkout.go
@@ -13,3 +13,18 @@ func (s *Server) CheckoutShowHandler(c echo.Context) error {
 
 	return Render(c, views.CheckoutShowView())
 }
+
+func (s *Server) CheckoutCreateHandler(c echo.Context) error {
+	wishlistId := c.FormValue("wishlist_id")
+	itemId := c.FormValue("wishlist_item_id")
+
+	slog.Info("checkout show handler", slog.String("wishlist_id", wishlistId), slog.String("wishlist_item_id", itemId))
+
+	// create checkout database records
+	// redirect to show view
+
+	return Render(c, views.CheckoutShowView())
+}
+
+// how does a user cancel a checkout?
+// maybe we need a confirmation page?
---
+++ queries/checkout.sql
@@ -0,0 +1,11 @@
+-- name: CreateCheckout :exec
+INSERT INTO checkout (id, wishlist_id, created_at, updated_at)
+VALUES (?, ?, ?, ?);
+
+-- name: CreateCheckoutItem :exec
+INSERT INTO checkout_item (id, checkout_id, wishlist_item_id, quantity, created_at, updated_at)
+VALUES (?, ?, ?, ?, ?, ?);
+
+-- name: CreateCheckoutResponse :exec
+INSERT INTO checkout_response (id, checkout_id, name, address_line_one, address_line_two, city, state, zip, message, created_at, updated_at)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
